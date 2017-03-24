893992c911c72b2127e9d10599f8228b86fe689a
Parth Oza <plusparth@gmail.com>
2017-03-24 18:38:22 -0400

    Force reload from server, not cache

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
+function reloadPage() {
+	location.reload(true);
+}
+
 function restartService()
 {
-    setTimeout(location.reload, 3000);
+    setTimeout(reloadPage, 3000);
     jQuery.get("/restart", function(data) {
     	console.log("setting reload...");
     })
